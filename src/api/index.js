d2ade931ebe11e3b154f04c92b89ea3215ddedca
Shubham Jain <shubhamjain2908@gmail.com>
2020-04-15 02:14:16 +0530

    syntax modified

--- src/api/index.js
+++ src/api/index.js
@@ -4,14 +4,8 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const { data } = await axios.get(url);
-        const modifiedData = {
-            confirmed: data.confirmed,
-            recovered: data.recovered,
-            deaths: data.deaths,
-            lastUpdate: data.lastUpdate
-        }
-        return modifiedData;
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
         console.error(error);
     }
