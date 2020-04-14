53b7820fa5a2f180a48b4d7bc05017beba324818
Shubham Jain <shubhamjain2908@gmail.com>
2020-04-15 02:09:29 +0530

    getting data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { fetchData } from './api';
 import styles from './App.module.css';
 import { Cards, Chart, CountryPicker } from './components';
 
 class App extends React.Component {
+    async componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
+
     render() {
         return (
             <div className={styles.container}>
