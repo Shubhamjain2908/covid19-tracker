e786b45c901fd8dd8ed90b6fd3790ca4d977777d
Shubham Jain <Shubhamjain2908@gmail.com>
2020-04-27 17:40:55 +0530

    Updates

---
+++ precache-manifest.4a1e3116c5c9c608d101dda7e3d11897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab89485bb905e5771c2a03cd18e13eac",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "daff6ad9ec4268ea2cdc",
+    "url": "/covid19-tracker/static/css/main.d9d22496.chunk.css"
+  },
+  {
+    "revision": "be1068d5d647280184fe",
+    "url": "/covid19-tracker/static/js/2.6495f3ee.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.6495f3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daff6ad9ec4268ea2cdc",
+    "url": "/covid19-tracker/static/js/main.035504fc.chunk.js"
+  },
+  {
+    "revision": "eb5ff0e51105f2f9eece",
+    "url": "/covid19-tracker/static/js/runtime-main.782e1f46.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/covid19.d7265326.png"
+  }
+]);
